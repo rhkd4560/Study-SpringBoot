6a168b8bf929ddec56e58dbee366873423b3f1c5
rhkd4560 <rhkd4560@naver.com>
2019-01-08 17:17:18 +0900

    Hello World

--- community/src/test/java/com/community/CommunityApplicationTests.java
+++ community/src/test/java/com/community/CommunityApplicationTests.java
@@ -9,9 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class CommunityApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
 
